31276957d847aa0d2738f379eeedbe6d067047ff
Marioly Cantillana <44650254+molisu@users.noreply.github.com>
2022-11-18 13:19:28 -0300

    ejercicio tipos

---
+++ ejercicio1/src/com/exercise/Main.java
@@ -0,0 +1,37 @@
+package com.exercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Enteros
+        byte number1 = 5;
+        System.out.println(number1);
+        short number2 = 10;
+        System.out.println(number2);
+        int number3 = 30;
+        System.out.println(number3);
+        long number4 = 100;
+        System.out.println(number4);
+        long phoneNumber = 999999999;
+        System.out.println(phoneNumber);
+
+        // Decimales
+        float decimal1 = 5.5f;
+        System.out.println(decimal1);
+        double decimal2 = 10.5d;
+        System.out.println(decimal2);
+
+        // Boolean
+        boolean variableF = false;
+        System.out.println(variableF);
+        boolean variableT = true;
+        System.out.println(variableT);
+
+        // Texto
+        char caracter = 'a';
+        System.out.println(caracter);
+        String cadena = "sdsad";
+        System.out.println(cadena);
+    }
+}
